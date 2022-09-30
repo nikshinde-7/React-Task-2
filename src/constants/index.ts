ab2b37fcb6238bd3ac54ed1b9389f7d1c43e4f3d
nikshinde-7 <shindenikhil336@gmail.com>
2022-09-30 15:07:34 +0530

    Fea - Added the constant file, Added types for components

---
+++ src/constants/index.ts
@@ -0,0 +1,9 @@
+const TEXT_LABEL = "Enter Text : ";
+const DATE_LABEL = "Select Date : "
+const RANGE_LABEL = "Select Range : "
+
+export {
+  TEXT_LABEL,
+  DATE_LABEL,
+  RANGE_LABEL
+}
---
+++ src/utils/data.ts
@@ -0,0 +1,29 @@
+export const optionsArray = [
+  {
+    value: 'Hampi',
+    label: 'Hampi'
+  },
+  {
+    value: 'Sandhan Valley',
+    label: 'Sandhan Valley'
+  },
+  {
+    value: 'Kaas Plataeu',
+    label: 'Kaas Plataeu'
+  }
+];
+
+export const genderList = [
+  {
+    gender: 'Male',
+    value: 'Male'
+  },
+  {
+    gender: 'Female',
+    value: 'Female'
+  },
+  {
+    gender: 'Other',
+    value: 'Other'
+  }
+];
