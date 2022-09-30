fded1dc0d1e74ff0b30ccf0ea5814b836cd470fc
nikshinde-7 <shindenikhil336@gmail.com>
2022-09-30 15:07:05 +0530

    Fea -  Added The required components for input, select & radio boxes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { InputTypeComponent } from './components/InputTypeComponent';
+import { RadioBox } from './components/RadioBoxComponent';
+import { SelectBox } from './components/SelectBoxComponent';
+import { DATE_LABEL, RANGE_LABEL, TEXT_LABEL } from './constants';
 
 function App() {
+
+  const [inputTextValue, setInputTextValue] = useState('');
+  const [inputFileValue, setInputFileValue] = useState('');
+  const [inputDateValue, setInputDateValue] = useState('');
+  const [inputSelectValue, setInputSelectValue] = useState('');
+  const [inputRadioValue, setInputRadioValue] = useState('');
+  const [inputRangeValue, setInputRangeValue] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h2> Test task - 2</h2>
+
+      <InputTypeComponent 
+        labelText={TEXT_LABEL} 
+        type="text" 
+        value={inputTextValue} 
+        handleChange={(e) => 
+        setInputTextValue(e.target.value)} 
+      />
+
+      <InputTypeComponent 
+        type="file" 
+        value={inputFileValue} 
+        handleChange={(e) => setInputFileValue(e.target.value)} 
+      />
+
+      <InputTypeComponent 
+        labelText={DATE_LABEL} 
+        type="date" 
+        value={inputDateValue} 
+        handleChange={(e) => setInputDateValue(e.target.value)} 
+      />
+
+      <SelectBox 
+        value={inputSelectValue} 
+        handleChange={(e) => setInputSelectValue(e.target.value)}
+      />
+
+      <RadioBox 
+        handleChange={(e) => setInputRadioValue(e.target.value)} 
+      />
+
+      <InputTypeComponent 
+        labelText={RANGE_LABEL} 
+        type="range" 
+        value={inputRangeValue} 
+        handleChange={(e) => setInputRangeValue(e.target.value)} 
+      />
+
+      <div>
+        <h2> Selected Values :</h2>
+        <h3>  
+          {inputTextValue} <br /> 
+          {inputFileValue} <br />
+          {inputDateValue} <br />
+          {inputSelectValue} <br />
+          {inputRadioValue} <br />
+          {inputRangeValue}
+        </h3>
+      </div>
+
     </div>
+
   );
 }
 
